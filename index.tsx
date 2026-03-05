5ebafa521f095952951d9b45fca2bcfda9cbfacd
pafferreira <pafferreira@gmail.com>
2026-03-05 09:52:52 -0300

    feat: Add Vercel Speed Insights

--- index.tsx
+++ index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { SpeedInsights } from "@vercel/speed-insights/react";
 import { polyfill } from "mobile-drag-drop";
 import { scrollBehaviourDragImageTranslateOverride } from "mobile-drag-drop/scroll-behaviour";
 import "mobile-drag-drop/default.css";
@@ -22,5 +23,6 @@ const root = ReactDOM.createRoot(rootElement);
 root.render(
   <React.StrictMode>
     <App />
+    <SpeedInsights />
   </React.StrictMode>
 );
